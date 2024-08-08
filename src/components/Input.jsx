6302261af5d35ed23523eb0d9a391a2a83fb8696
SudheerVgts <sudheer@vishgyana.com>
2024-08-08 16:00:06 +0530

    ..

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -23,21 +23,6 @@ export default function Input({ onSend, disabled }) {
           placeholder="Enter your message here"
           disabled={disabled}
         />
-        <button disabled={text === ""}>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 500 500"
-          >
-            <g>
-              <g>
-                <polygon points="0,497.25 535.5,267.75 0,38.25 0,216.75 382.5,267.75 0,318.75" />
-              </g>
-            </g>
-          </svg>
-        </button>
       </form>
     </div>
   );
